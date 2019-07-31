1f61bf235da12f77a6ef5e7386c8c409c92bf2a8
lazynessmind <lazynessmind@gmail.com>
2019-07-31 17:30:37 +0100

    added enchant to harvest a 3 by 3 area

---
+++ src/main/java/com/lazynessmind/farmingtools/init/FarmingToolsEnchants.java
@@ -0,0 +1,19 @@
+package com.lazynessmind.farmingtools.init;
+
+import com.lazynessmind.farmingtools.init.enchants.EnchantHoeHarvest3x3;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.item.ItemHoe;
+import net.minecraftforge.common.util.EnumHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FarmingToolsEnchants {
+
+    public static final EnumEnchantmentType HOE = EnumHelper.addEnchantmentType("hoe", (item)->(item instanceof ItemHoe));
+
+    public static final List<Enchantment> ENCHANTS = new ArrayList<>();
+
+    public static final Enchantment HOEHARVEST = new EnchantHoeHarvest3x3();
+}
---
+++ src/main/java/com/lazynessmind/farmingtools/init/enchants/EnchantHoeHarvest3x3.java
@@ -0,0 +1,38 @@
+package com.lazynessmind.farmingtools.init.enchants;
+
+import com.lazynessmind.farmingtools.init.FarmingToolsEnchants;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemStack;
+
+public class EnchantHoeHarvest3x3 extends Enchantment {
+
+    public EnchantHoeHarvest3x3() {
+        super(Rarity.RARE, FarmingToolsEnchants.HOE, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND});
+        setRegistryName("3x3harvest");
+        setName("3x3harvest");
+
+        FarmingToolsEnchants.ENCHANTS.add(this);
+    }
+
+    @Override
+    public int getMinEnchantability(int enchantmentLevel) {
+        return 20 * enchantmentLevel;
+    }
+
+    @Override
+    public int getMaxEnchantability(int enchantmentLevel) {
+        return getMinEnchantability(enchantmentLevel) + 10;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public boolean canApply(ItemStack stack) {
+        return stack.getItem() instanceof ItemHoe;
+    }
+}
