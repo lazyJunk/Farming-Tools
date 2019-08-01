dd3d9e3eaec6cc432a8b03ab80468d8f7edf08cc
lazynessmind <lazynessmind@gmail.com>
2019-08-01 02:31:45 +0100

    bug fixed

--- src/main/java/com/lazynessmind/farmingtools/container/ContainerHarvester.java
+++ src/main/java/com/lazynessmind/farmingtools/container/ContainerHarvester.java
@@ -6,6 +6,8 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 
@@ -27,6 +29,61 @@ public class ContainerHarvester extends Container {
         for (int x = 0; x < 9; x++) {
             this.addSlotToContainer(new Slot(inventoryPlayer, x, 8 + x * 18, 142));
         }
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = (Slot)this.inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (index != 0)
+            {
+                if (itemstack1.getItem() instanceof ItemHoe)
+                {
+                    if (!this.mergeItemStack(itemstack1, 0, 0+1, false))
+                    {
+                        return ItemStack.EMPTY;
+                    }
+                }
+                else if (index >= 1 && index < 28)
+                {
+                    if (!this.mergeItemStack(itemstack1, 28, 37, false))
+                    {
+                        return ItemStack.EMPTY;
+                    }
+                }
+                else if (index >= 28 && index < 37 && !this.mergeItemStack(itemstack1, 1, 28, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if (!this.mergeItemStack(itemstack1,1, 37, false))
+            {
+                return ItemStack.EMPTY;
+            }
+
+            if (itemstack1.getCount() == 0)
+            {
+                slot.putStack((ItemStack)ItemStack.EMPTY);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount())
+            {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(playerIn, itemstack1);
+        }
+        return itemstack;
     }
 
     @Override
