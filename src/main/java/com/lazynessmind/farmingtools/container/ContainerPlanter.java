7e54a1da662941e892c258c4cd88362e27312132
lazynessmind <lazynessmind@gmail.com>
2019-08-01 02:32:08 +0100

    bug fixed

--- src/main/java/com/lazynessmind/farmingtools/container/ContainerPlanter.java
+++ src/main/java/com/lazynessmind/farmingtools/container/ContainerPlanter.java
@@ -6,6 +6,10 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemSeedFood;
+import net.minecraft.item.ItemSeeds;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 
@@ -32,5 +36,60 @@ public class ContainerPlanter extends Container {
     @Override
     public boolean canInteractWith(EntityPlayer playerIn) {
         return tileEntityPlanter.isUsableByPlayer(playerIn);
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer playerIn, int index) {
+        ItemStack itemstack = ItemStack.EMPTY;
+        Slot slot = (Slot)this.inventorySlots.get(index);
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (index != 0)
+            {
+                if (itemstack1.getItem() instanceof ItemSeedFood || itemstack1.getItem() instanceof ItemSeeds)
+                {
+                    if (!this.mergeItemStack(itemstack1, 0, 0+1, false))
+                    {
+                        return ItemStack.EMPTY;
+                    }
+                }
+                else if (index >= 1 && index < 28)
+                {
+                    if (!this.mergeItemStack(itemstack1, 28, 37, false))
+                    {
+                        return ItemStack.EMPTY;
+                    }
+                }
+                else if (index >= 28 && index < 37 && !this.mergeItemStack(itemstack1, 1, 28, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if (!this.mergeItemStack(itemstack1,1, 37, false))
+            {
+                return ItemStack.EMPTY;
+            }
+
+            if (itemstack1.getCount() == 0)
+            {
+                slot.putStack((ItemStack)ItemStack.EMPTY);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.getCount() == itemstack.getCount())
+            {
+                return ItemStack.EMPTY;
+            }
+
+            slot.onTake(playerIn, itemstack1);
+        }
+        return itemstack;
     }
 }
