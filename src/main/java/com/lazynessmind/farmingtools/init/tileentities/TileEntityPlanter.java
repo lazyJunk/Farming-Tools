516e918e9135505dce58a72b17d84e043b5b0103
lazynessmind <lazynessmind@gmail.com>
2019-08-09 04:16:59 +0100

    added redstone powered state if the user want to stop the work

--- src/main/java/com/lazynessmind/farmingtools/init/tileentities/TileEntityPlanter.java
+++ src/main/java/com/lazynessmind/farmingtools/init/tileentities/TileEntityPlanter.java
@@ -1,16 +1,12 @@
 package com.lazynessmind.farmingtools.init.tileentities;
 
+import com.lazynessmind.farmingtools.interfaces.IRange;
+import com.lazynessmind.farmingtools.interfaces.IRedPower;
 import com.lazynessmind.farmingtools.util.FarmUtils;
-import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.SPacketUpdateTileEntity;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.IPlantable;
@@ -20,43 +16,61 @@ import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nullable;
 
-public class TileEntityPlanter extends FTTileEntity implements ITickable {
+public class TileEntityPlanter extends FTTileEntity implements ITickable, IRange, IRedPower {
 
+    //Tile Data
+    public int range = 1;
+    private boolean showRangeArea = false;
+    private boolean needRedstonePower = false;
     private ItemStackHandler handler = new ItemStackHandler(1);
+
+    public int yRange = 1;
     private IBlockState crop;
     private int doWorkStartTime;
     private int doWorkEndTime = 10;
 
     @Override
-    public NBTTagCompound writeNBT(NBTTagCompound compound) {
+    public void writeNBT(NBTTagCompound compound) {
+        super.writeNBT(compound);
+
         compound.setTag("Items", this.handler.serializeNBT());
-        return compound;
+        compound.setBoolean("ShowRangeArea", this.showRangeArea);
+        compound.setBoolean("NeedRedstonePower", this.needRedstonePower);
+        compound.setInteger("Range", this.range);
     }
 
     @Override
     public void readNBT(NBTTagCompound compound) {
+        super.readNBT(compound);
+
         this.handler.deserializeNBT(compound.getCompoundTag("Items"));
+        this.showRangeArea = compound.getBoolean("ShowRangeArea");
+        this.needRedstonePower = compound.getBoolean("NeedRedstonePower");
+        this.range = compound.getInteger("Range");
     }
 
     @Override
     public boolean hasCapability(Capability<?> capability, @Nullable EnumFacing facing) {
-        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) return true;
-        return false;
+        return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY;
     }
 
     @Override
     public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
-        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) return (T) this.handler;
+        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return (T) this.handler;
+        }
         return super.getCapability(capability, facing);
     }
 
     @Override
     public void update() {
-        if (!handler.getStackInSlot(0).isEmpty()) {
-            crop = ((IPlantable) handler.getStackInSlot(0).getItem()).getPlant(world, pos);
-        }
-        if(doWorkStartTime < doWorkEndTime){
-            doWorkStartTime++;
+        if (!world.isRemote) {
+            if (!handler.getStackInSlot(0).isEmpty()) {
+                crop = ((IPlantable) handler.getStackInSlot(0).getItem()).getPlant(world, pos);
+            }
+            if (doWorkStartTime < doWorkEndTime) {
+                doWorkStartTime++;
+            }
         }
 
         plantCrop(crop);
@@ -64,13 +78,23 @@ public class TileEntityPlanter extends FTTileEntity implements ITickable {
 
     private void plantCrop(IBlockState crop) {
         if (!world.isRemote) {
-            for (BlockPos pos : FarmUtils.checkInRange(4, getPos(), 1, false)) {
+            for (BlockPos pos : FarmUtils.checkInRange(range, getPos(), yRange, false)) {
                 if (FarmUtils.canPlantCrop(pos, world)) {
                     if (crop != null && !handler.getStackInSlot(0).isEmpty()) {
-                        if(doWork()){
-                            world.setBlockState(pos, crop);
-                            handler.extractItem(0, 1, false);
-                            doWorkStartTime = 0;
+                        if(needRedstonePower()){
+                            if(world.isBlockPowered(pos)){
+                                if(doWork()){
+                                    world.setBlockState(pos, crop);
+                                    handler.extractItem(0, 1, false);
+                                    doWorkStartTime = 0;
+                                }
+                            }
+                        } else {
+                            if(doWork()){
+                                world.setBlockState(pos, crop);
+                                handler.extractItem(0, 1, false);
+                                doWorkStartTime = 0;
+                            }
                         }
                     }
                 }
@@ -82,11 +106,35 @@ public class TileEntityPlanter extends FTTileEntity implements ITickable {
         return this.world.getTileEntity(this.pos) == this && player.getDistanceSq((double) this.pos.getX() + 0.5D, (double) this.pos.getY() + 0.5D, (double) this.pos.getZ() + 0.5D) <= 64.0D;
     }
 
-    private boolean doWork(){
+    private boolean doWork() {
         return doWorkStartTime >= doWorkEndTime;
     }
 
     public ItemStackHandler getHandler() {
         return handler;
+    }
+
+    //IRange implementations
+    @Override
+    public void showRangeArea(boolean show) {
+        this.showRangeArea = show;
+        markDirty();
+    }
+
+    @Override
+    public boolean canShowRangeArea() {
+        return this.showRangeArea;
+    }
+
+    //IRedPower implementations
+    @Override
+    public void setNeedRedstonePower(boolean need) {
+        this.needRedstonePower = need;
+        markDirty();
+    }
+
+    @Override
+    public boolean needRedstonePower() {
+        return needRedstonePower;
     }
 }
