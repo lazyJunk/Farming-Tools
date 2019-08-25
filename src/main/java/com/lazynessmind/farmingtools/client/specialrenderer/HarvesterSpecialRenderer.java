7a10f657311c05c3013cd88d244104ffe406e320
lazynessmind <lazynessmind@gmail.com>
2019-08-25 18:25:40 +0100

    25/8 update 2

--- src/main/java/com/lazynessmind/farmingtools/client/specialrenderer/HarvesterSpecialRenderer.java
+++ src/main/java/com/lazynessmind/farmingtools/client/specialrenderer/HarvesterSpecialRenderer.java
@@ -1,47 +1,13 @@
 package com.lazynessmind.farmingtools.client.specialrenderer;
 
+import com.lazynessmind.farmingtools.init.tileentities.TileEntityGrowthPedestal;
 import com.lazynessmind.farmingtools.init.tileentities.TileEntityHarvester;
-import com.lazynessmind.farmingtools.util.RenderUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.item.ItemStack;
 
-public class HarvesterSpecialRenderer extends TileEntitySpecialRenderer<TileEntityHarvester> {
-
-    private EntityItem entityItem = new EntityItem(Minecraft.getMinecraft().world, 0, 0, 0, ItemStack.EMPTY);
-
-    private float yaw = 0f;
+public class HarvesterSpecialRenderer extends PedestalSpecialRenderer<TileEntityHarvester> {
 
     @Override
     public void render(TileEntityHarvester te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
-        if(yaw < 360f){
-            yaw += 1f;
-        } else if(yaw >= 360f){
-            yaw = 0f;
-        }
-
-        entityItem.setItem(te.getHandler().getStackInSlot(0));
-
-        if(te.canShowRangeArea()) {
-            this.setLightmapDisabled(true);
-            Tessellator tessellator = Tessellator.getInstance();
-            BufferBuilder bufferbuilder = tessellator.getBuffer();
-            RenderUtils.renderBox(bufferbuilder, (x - te.getRange()), (y -te.getVerticalRange()), (z - te.getRange()), (x + te.getRange() + 1), (y + te.getVerticalRange()), (z + te.getRange() + 1), 255, 255, 255,2f, tessellator);
-            this.setLightmapDisabled(false);
-        }
-
-        GlStateManager.pushMatrix();
-        {
-            GlStateManager.translate(x, y, z);
-            GlStateManager.scale(0.5f, 0.5f, 0.5f);
-            GlStateManager.translate(1f, 1.5f, 1f);
-            GlStateManager.rotate(yaw, 0f, 0.1f, 0f);
-            Minecraft.getMinecraft().getRenderManager().renderEntity(entityItem, 0, 0, 0, yaw, 0f, false);
-        }
-        GlStateManager.popMatrix();
+        renderItemOnTop(te, x, y, z);
+        renderRangeArea(te, x, y, z);
     }
 }
--- src/main/java/com/lazynessmind/farmingtools/client/specialrenderer/PlanterSpecialRenderer.java
+++ src/main/java/com/lazynessmind/farmingtools/client/specialrenderer/PlanterSpecialRenderer.java
@@ -1,48 +1,12 @@
 package com.lazynessmind.farmingtools.client.specialrenderer;
 
 import com.lazynessmind.farmingtools.init.tileentities.TileEntityPlanter;
-import com.lazynessmind.farmingtools.util.RenderUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.item.ItemStack;
 
-public class PlanterSpecialRenderer extends TileEntitySpecialRenderer<TileEntityPlanter> {
-
-    private EntityItem entityItem = new EntityItem(Minecraft.getMinecraft().world, 0, 0, 0, ItemStack.EMPTY);
-
-    private float yaw = 0f;
-
+public class PlanterSpecialRenderer extends PedestalSpecialRenderer<TileEntityPlanter> {
 
     @Override
     public void render(TileEntityPlanter te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
-        if (yaw < 360f) {
-            yaw += 1f;
-        } else if (yaw >= 360f) {
-            yaw = 0f;
-        }
-
-        entityItem.setItem(te.getHandler().getStackInSlot(0));
-
-        if(te.canShowRangeArea()) {
-            this.setLightmapDisabled(true);
-            Tessellator tessellator = Tessellator.getInstance();
-            BufferBuilder bufferbuilder = tessellator.getBuffer();
-            RenderUtils.renderBox(bufferbuilder, (x - te.getRange()), y, (z - te.getRange()), (x + te.getRange() + 1), y+1, (z + te.getRange() + 1), 255, 255, 255,2f, tessellator);
-            this.setLightmapDisabled(false);
-        }
-
-        GlStateManager.pushMatrix();
-        {
-            GlStateManager.translate(x, y, z);
-            GlStateManager.scale(0.5f, 0.5f, 0.5f);
-            GlStateManager.translate(1f, 1.5f, 1f);
-            GlStateManager.rotate(yaw, 0f, 0.1f, 0f);
-            Minecraft.getMinecraft().getRenderManager().renderEntity(entityItem, 0, 0, 0, yaw, 0f, false);
-        }
-        GlStateManager.popMatrix();
+        renderItemOnTop(te, x, y, z);
+        renderRangeArea(te, x, y, z);
     }
 }
