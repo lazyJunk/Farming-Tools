d22ff8522ddaf9345819b521a27dc8d2c8c52398
lazynessmind <lazynessmind@gmail.com>
2019-11-05 03:44:32 +0000

    redone and working

--- src/main/java/com/lazynessmind/farmingtools/block/tileentities/TileEntityHarvester.java
+++ src/main/java/com/lazynessmind/farmingtools/block/tileentities/TileEntityHarvester.java
@@ -2,30 +2,39 @@ package com.lazynessmind.farmingtools.block.tileentities;
 
 import com.lazynessmind.farmingtools.block.tileentities.base.TileEntityPedestal;
 import com.lazynessmind.farmingtools.util.FarmUtils;
-import com.lazynessmind.farmingtools.util.UpgradeUtil;
+import com.lazynessmind.farmingtools.util.TypeUtil;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.client.Minecraft;
-import net.minecraft.init.Items;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 
 public class TileEntityHarvester extends TileEntityPedestal implements ITickable {
 
+    private int timer;
+
     public TileEntityHarvester() {
-        super(false, false, UpgradeUtil.getMaxCooldownFromType(0), UpgradeUtil.getRangeFromType(0), 3, Items.DIAMOND_HOE);
+        super(false, false, 3);
     }
 
     @Override
     public void update() {
         setType(getBlockMetadata());
-        setRange(UpgradeUtil.getRangeFromType(getBlockMetadata()));
-        harvestBlock(pos);
+        setRange(TypeUtil.getHorizontalRangeFromType(getType()));
+        setTimeBetween(TypeUtil.getTimeBetweenFromType(getType()));
+        if (!world.isRemote) {
+            if (timer < getTimeBetween()) {
+                timer++;
+            } else if (timer >= getTimeBetween()) {
+                timer = 0;
+                harvestBlock(pos);
+            }
+        }
         this.markDirty();
     }
 
     private void harvestBlock(BlockPos pos) {
-        if (!world.isRemote && !mainSlot().isEmpty()) {
-            for (BlockPos poss : FarmUtils.checkInRange(getRange(), pos, getVerticalRange(), false)) {
+        if (!mainSlot().isEmpty()) {
+            for (BlockPos poss : FarmUtils.checkInRange(getRange(), pos, 3, false)) {
                 if (world.getBlockState(poss).getBlock() instanceof BlockCrops) {
                     BlockCrops crops = (BlockCrops) world.getBlockState(poss).getBlock();
                     if (needRedstonePower()) {
@@ -35,7 +44,6 @@ public class TileEntityHarvester extends TileEntityPedestal implements ITickable
                                 if (mainSlot().isItemStackDamageable()) {
                                     mainSlot().damageItem(1, Minecraft.getMinecraft().player);
                                 }
-
                             }
                         }
                     } else {
