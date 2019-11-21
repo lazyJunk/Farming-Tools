bda6240b08f6bee79531d7e4ec3c45c436638d13
lazynessmind <lazynessmind@gmail.com>
2019-11-21 08:58:18 +0000

    - Reset change when destroyed

--- src/main/java/com/lazynessmind/farmingtools/block/BlockNatureGather.java
+++ src/main/java/com/lazynessmind/farmingtools/block/BlockNatureGather.java
@@ -4,12 +4,21 @@ import com.lazynessmind.farmingtools.block.tileentities.TileEntityNatureGather;
 import com.lazynessmind.farmingtools.block.tileentities.base.FTBlockTileEntity;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class BlockNatureGather extends FTBlockTileEntity<TileEntityNatureGather> {
 
     public BlockNatureGather(String name) {
         super(Material.IRON, name, true, false);
+    }
+
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+        TileEntityNatureGather.resetCurrentAmount();
     }
 
     @Override
